0956df642769854ce7cc53950945f48a1c2800c6
changho1531 <munsik8@naver.com>
2024-11-10 00:07:08 +0900

    MMO Unity
    깃허브~

---
+++ Assets/Scripts/Managers/ResourceManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager
+{
+    public T Load<T>(string path) where T : Object
+    {
+        if(typeof(T) == typeof(GameObject))
+        {
+            string name = path;
+            int index = name.LastIndexOf('/');
+            if(index >= 0 )
+                name = name.Substring(index + 1);
+
+            GameObject go = Managers.Pool.GetOriginal(name);
+            if (go != null)
+                return go as T;
+        }
+
+        return Resources.Load<T>(path);
+    }
+
+    public GameObject Instantiate(string path, Transform parent = null)
+    {   
+        GameObject original = Load<GameObject>($"Prefabs/{path}");
+        if (original == null)
+        {
+            Debug.Log($"Failed to load prefab : {path}");
+            return null;
+        }
+
+        // 2. 혹시 폴링된 애가 있을까
+        GameObject go = Object.Instantiate(original, parent);
+        go.name = original.name;
+
+        return go;
+    }
+
+    public void Destroy(GameObject go)
+    {
+        if (go == null)
+            return;
+
+        // 만약에 풀링이 필요한 아이라면 -> 풀링 매니저에 위탁
+        Object.Destroy(go);
+    }
+}
